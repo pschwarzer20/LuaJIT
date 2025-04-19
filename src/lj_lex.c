adac56c97c89f669d8fb4ff0331fcb79ce624532
Patrick Schwarzer <79915618+pschwarzer20@users.noreply.github.com>
2025-04-19 17:28:55 +0200

    Added !=, && and || operators

--- src/lj_lex.c
+++ src/lj_lex.c
@@ -360,6 +360,15 @@ static LexToken lex_scan(LexState *ls, TValue *tv)
     case '~':
       lex_next(ls);
       if (ls->c != '=') return '~'; else { lex_next(ls); return TK_ne; }
+    case '!':
+      lex_next(ls);
+      if (ls->c != '=') return TK_not; else { lex_next(ls); return TK_ne; }
+    case '&':
+      lex_next(ls);
+      if (ls->c != '&') return '&'; else { lex_next(ls); return TK_and; }
+    case '|':
+      lex_next(ls);
+      if (ls->c != '|') return '|'; else { lex_next(ls); return TK_or; }
     case ':':
       lex_next(ls);
       if (ls->c != ':') return ':'; else { lex_next(ls); return TK_label; }
